5ef9222e7941df8cf8221c4dfcd1ad7472c088f2
zhaotianff <zhaotianff@163.com>
2018-08-07 09:28:07 +0800

    Update Lib

--- AliyunOssHelperLib/AliyunOssHelperLib/OssHelper.cs
+++ AliyunOssHelperLib/AliyunOssHelperLib/OssHelper.cs
@@ -7,6 +7,7 @@ using Aliyun.OSS;
 using Aliyun.OSS.Common;
 using Aliyun.OSS.Util;
 using System.IO;
+using System.Threading;
 
 namespace ZtiLib
 {
@@ -20,6 +21,7 @@ namespace ZtiLib
         private bool isCNameFlag = false;
 
         private OssClient client;
+        AutoResetEvent resetEvent;
 
         /// <summary>
         /// AccessKeyID
@@ -128,6 +130,7 @@ namespace ZtiLib
             isCNameFlag = isCName;
 
             client = new OssClient(endpoint, accessKeyID, accessKeySecret, conf);
+            resetEvent= new AutoResetEvent(false);
         }
 
         #endregion
@@ -140,6 +143,17 @@ namespace ZtiLib
         /// <param name="bucketName">BucketName</param>
         /// <returns>result</returns>
         public bool CreateBucket(string bucketName)
+        {
+            this.bucketName = bucketName;
+            return CreateBucket();
+        }
+
+        /// <summary>
+        /// CreateBucket
+        /// </summary>
+        /// <param name="bucketName">BucketName</param>
+        /// <returns>result</returns>
+        public bool CreateBucket()
         {
             try
             {
@@ -159,7 +173,7 @@ namespace ZtiLib
         /// <returns></returns>
         public CannedAccessControlList GetBucketAcl()
         {
-            AccessControlList acl = null;
+            AccessControlList acl = new AccessControlList();
             try
             {
                 acl = client.GetBucketAcl(bucketName);
@@ -236,9 +250,9 @@ namespace ZtiLib
         /// <summary>
         /// Set Bucket Acl
         /// </summary>
-        public bool SetBucketAcl(string _bucketName, CannedAccessControlList accessType)
+        public bool SetBucketAcl(string bucketName, CannedAccessControlList accessType)
         {
-            this.bucketName = _bucketName;
+            this.bucketName = bucketName;
             return SetBucketAcl(accessType);
         }
 
@@ -262,9 +276,9 @@ namespace ZtiLib
         /// <summary>
         /// Delete Bucket
         /// </summary>
-        public bool DeleteBucket(string _bucketName)
+        public bool DeleteBucket(string bucketName)
         {
-            this.bucketName = _bucketName;
+            this.bucketName = bucketName;
             return DeleteBucket();
         }
 
@@ -331,16 +345,160 @@ namespace ZtiLib
         /// Upload file object to bucket
         /// </summary>
         /// <param name="filePath">File to upload</param>
-        /// <param name="_bueketName">Bucket name</param>
+        /// <param name="objectKey">Object key in oss</param>
         /// <returns></returns>
-        public bool PutFile(string _bueketName,string filePath)
+        public bool PutFile(string filePath,string objectKey)
         {
-            this.bucketName = _bueketName;
-            return PutFile(filePath);
+            try
+            {
+                client.PutObject(bucketName, objectKey, filePath);
+                return true;
+            }
+            catch (OssException ex)
+            {
+                lastError = ex;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Upload file object to bucket
+        /// </summary>
+        /// <param name="filePath">File to upload</param>
+        /// <param name="_bueketName">Bucket name</param>
+        /// <param name="objectKey">Object key in oss</param>
+        /// <returns></returns>
+        public bool PutFile(string bueketName, string filePath,string objectKey)
+        {
+            this.bucketName = bueketName;
+            return PutFile(filePath,objectKey);
+        }
+
+        /// <summary>
+        /// Put File With Md5 Check
+        /// </summary>
+        /// <param name="filePath">File to upload</param>
+        /// <param name="objectKey">Object key in oss</param>
+        /// <param name="md5">md5 code</param>
+        /// <returns></returns>
+        public bool PutFileWithMd5(string filePath, string objectKey,string md5)
+        {
+            try
+            {
+                using (FileStream fs = File.Open(filePath, FileMode.Open))
+                {
+                    md5 = OssUtils.ComputeContentMd5(fs, fs.Length);
+                }
+                ObjectMetadata objectMeta = new ObjectMetadata
+                {
+                    ContentMd5 = md5
+                };
+                client.PutObject(bucketName, objectKey, filePath, objectMeta);
+                return true;
+            }
+            catch (OssException ex)
+            {
+                lastError = ex;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Put File With Md5 Check
+        /// </summary>
+        /// <param name="bucketName">Bucket name</param>
+        /// <param name="filePath">File to upload</param>
+        /// <param name="objectKey">Object key in oss</param>
+        /// <param name="md5">md5 code</param>
+        /// <returns></returns>
+        public bool PutFileWithMd5(string bucketName,string filePath, string objectKey, string md5)
+        {
+            this.bucketName = bucketName;
+            return PutFileWithMd5(filePath, objectKey, md5);
+        }
+
+        //TODO
+        //PutFileWithHeader()
+
+        /// <summary>
+        /// Pub File Async
+        /// </summary>
+        /// <param name="bucketName">Bucket Name</param>
+        public void PutFileAsync(string bucketName,string filePath,string objcetKey)
+        {
+            try
+            {
+                resetEvent.Reset();
+
+                using (var fs = File.Open(filePath, FileMode.Open))
+                {
+                    string result = "Notice user: put object finish";
+                    ObjectMetadata metadata = new ObjectMetadata();
+                    client.BeginPutObject(bucketName, objcetKey, fs, metadata, PutFileCallback, result.ToCharArray());
+                    resetEvent.WaitOne();
+                }
+            }
+            catch (OssException ex)
+            {
+                lastError = ex;
+            }
+        }
+
+        /// <summary>
+        /// Put File Async Callback 
+        /// </summary>
+        /// <param name="ar">IAsyncResult</param>
+        private void PutFileCallback(IAsyncResult ar)
+        {
+            try
+            {
+                client.EndPutObject(ar);              
+            }
+            catch (OssException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                resetEvent.Set();
+            }
         }
         #endregion
 
         #region Download
+
+        #endregion
+
+        #region Object Management
+        /// <summary>
+        /// Delete object
+        /// </summary>
+        /// <param name="key">object name</param>
+        /// <remarks>删除文件夹需要在后面加个"/"</remarks>
+        public bool DeleteObject(string name)
+        {
+            try
+            {
+                client.DeleteObject(bucketName, name);
+                return true;
+            }
+            catch (OssException ex)
+            {
+                lastError = ex;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Delete object
+        /// </summary>
+        /// <param name="bucketName">Bucket name</param>
+        /// <param name="key">object name</param>
+        public bool DeleteObject(string bucketName, string name)
+        {
+            this.bucketName = bucketName;
+            return DeleteObject(name);
+        }
         #endregion
     }
 }
